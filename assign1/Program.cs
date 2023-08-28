76c91eec43c6aeaf420b93b039303c8769e5139b
dharm <dharm@LAPTOP-OROP1HV7>
2023-08-28 14:07:54 +0530

    Add project files.

---
+++ assign1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class assign1
+{
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double Salary { get; set; }
+
+    public assign1(string name, int age, double salary)
+    {
+        Name = name;
+        Age = age;
+        Salary = salary;
+    }
+
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Age: {Age}");
+        Console.WriteLine($"Salary: ${Salary}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        assign1 employee1 = new assign1("John Doe", 30, 50000.0);
+
+
+        employee1.DisplayDetails();
+    }
+}
+
